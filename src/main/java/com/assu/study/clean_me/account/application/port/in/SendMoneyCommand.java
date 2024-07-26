b714bcecb76916ca8c54dc0a97fea9ac3a4df0a5
assu10 <juhyun1010@naver.com>
2024-07-26 21:29:29 +0900

    3. 입력 유효성 검증: 입력 모델의 생성자

--- src/main/java/com/assu/study/clean_me/account/application/port/in/SendMoneyCommand.java
+++ src/main/java/com/assu/study/clean_me/account/application/port/in/SendMoneyCommand.java
@@ -2,13 +2,13 @@ package com.assu.study.clean_me.account.application.port.in;
 
 import com.assu.study.clean_me.account.domain.Account;
 import com.assu.study.clean_me.account.domain.Money;
+import com.assu.study.clean_me.common.SelfValidating;
 import jakarta.validation.constraints.NotNull;
-import lombok.EqualsAndHashCode;
 import lombok.Value;
 
+// 입력 모델
 @Value
-@EqualsAndHashCode
-public class SendMoneyCommand {
+public class SendMoneyCommand extends SelfValidating<SendMoneyCommand> {
   @NotNull private final Account.AccountId sourceAccountId;
 
   @NotNull private final Account.AccountId targetAccountId;
@@ -20,5 +20,11 @@ public class SendMoneyCommand {
     this.sourceAccountId = sourceAccountId;
     this.targetAccountId = targetAccountId;
     this.money = money;
+
+    if (!money.isPositive()) {
+      throw new IllegalArgumentException();
+    }
+    // 각 필드에 지정된 Bean Validation 애너테이션 검증
+    this.validateSelf();
   }
 }
