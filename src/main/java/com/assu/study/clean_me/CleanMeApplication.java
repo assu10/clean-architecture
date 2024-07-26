4921aa651d0521a41921d6d659f22570a54d1617
assu10 <juhyun1010@naver.com>
2024-07-26 12:01:53 +0900

    first

---
+++ src/main/java/com/assu/study/clean_me/CleanMeApplication.java
@@ -0,0 +1,13 @@
+package com.assu.study.clean_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanMeApplication.class, args);
+	}
+
+}
